1ac9ec18ff081c68291f912e6b7acbbe9a347167
ezakiev <57688582+ezakiev@users.noreply.github.com>
2021-06-22 18:53:21 +0300

    Add files via upload

---
+++ ChetNechetArray/ChetNechetArray/MyArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChetNechetArray
+{
+    class MyArray
+    {
+        private int[] array; // сам массив
+        private int n; // количество элементов
+        public MyArray() // инициализация полей и заполнение массива
+        {
+            n = 10;
+            array = new int[n];
+            FillRandomArray();
+        }
+
+        public MyArray(int n) // создание массива с определённым числом элементов
+        {
+            this.n = n;
+            array = new int[n];
+            FillRandomArray();
+        }
+
+        public int N => n; // св-во для получения числа элементов
+        public int[] Array => array; // св-во для получения массива
+
+        private void FillRandomArray() // метод случайного заполнения массива
+        {
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+                array[i] = rand.Next(1, 100);
+        }
+
+        public void Show() // печать массива в консоли
+        {
+            foreach (int el in array)
+                Console.Write(el + " ");
+            Console.WriteLine();
+        }
+
+        public void DoubleChetOnNechetElements() // метод удвоения чётных элементов, стоящих на нечётных местах
+        {
+            int counter = 0; // счётчик подходящих элементов
+            for (int i = 0; i < n; i += 2)
+                if (array[i] % 2 == 0)
+                {
+                    array[i] *= 2;
+                    counter++;
+                }
+            if (counter == 0)
+                Console.WriteLine("Нет чётных элементов, стоящих на нечётных местах");
+        }
+
+        public void MaxNechetOnChetElement() // метод поиска максимального нечётного элемента, стоящего на чётном месте
+        {
+            int max = -1; // начальный максимум
+            for (int i = 1; i < n; i += 2)
+                if (array[i] % 2 != 0 && array[i] > max)
+                    max = array[i];
+            if (max == -1)
+                Console.WriteLine("Нет нечётных элементов, стоящих на чётных местах");
+            else
+                Console.WriteLine(max);
+        }
+    }
+}
---
+++ ChetNechetArray/ChetNechetArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChetNechetArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Введение пользователем количества элементов
+            Console.Write("Введите количество элементов массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            MyArray array = new MyArray(n); // создание переменной массива и присвоение ей ссылки на экземпляр класса массива
+
+            Console.WriteLine('\n' + "Исходный массив:");
+            array.Show(); // вывод исходного массива
+
+            Console.WriteLine('\n' + "Полученный массив:");
+            array.DoubleChetOnNechetElements(); // удвоение чётных элементов, стоящих на нечётных местах
+            array.Show(); // вывод изменённого массива
+
+            Console.WriteLine('\n' + "Максимальный нечётный элемент, стоящий на чётном месте:");
+            array.MaxNechetOnChetElement(); // поиск и вывод максимального нечётного элемента, стоящего на чётном месте
+        }
+    }
+}
